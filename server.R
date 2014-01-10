df9ab335765929cad859ecfff26accab4f0ce85a
trestletech <jeff.allen@trestletechnology.net>
2014-01-10 13:51:26 -0600

    Fleshed out documentation.

--- server.R
+++ server.R
@@ -1,13 +1,16 @@
 library(shiny)
 library(stringr)
 
+# Globally define a place where all users can share some reactive data.
 vars <- reactiveValues(chat=NULL, users=NULL)
 
+# Restore the chat log from the last session.
 if (file.exists("chat.Rds")){
-  # Restore the chat log from a previous session.
   vars$chat <- readRDS("chat.Rds")
 }
 
+#' Get the prefix for the line to be added to the chat window. Usually a newline
+#' character unless it's the first line.
 linePrefix <- function(){
   if (is.null(isolate(vars$chat))){
     return("")
@@ -16,10 +19,14 @@ linePrefix <- function(){
 }
 
 shinyServer(function(input, output, session) {
+  # Create a spot for reactive variables specific to this particular session
   sessionVars <- reactiveValues(username = "")
   
+  # Track whether or not this session has been initialized. We'll use this to
+  # assign a username to unininitialized sessions.
   init <- FALSE
   
+  # When a session is ended, remove the user and note that they left the room. 
   session$onSessionEnded(function() {
     isolate({
       vars$users <- vars$users[vars$users != sessionVars$username]
@@ -29,7 +36,9 @@ shinyServer(function(input, output, session) {
     })
   })
   
+  # Observer to handle changes to the username
   observe({
+    # We want a reactive dependency on this variable, so we'll just list it here.
     input$user
     
     if (!init){
@@ -42,6 +51,7 @@ shinyServer(function(input, output, session) {
       })
       init <<- TRUE
     } else{
+      # A previous username was already given
       isolate({
         if (input$user == sessionVars$username || input$user == ""){
           # No change. Just return.
@@ -52,6 +62,7 @@ shinyServer(function(input, output, session) {
         # First, remove the old one
         vars$users <- vars$users[vars$users != sessionVars$username]
         
+        # Note the change in the chat log
         vars$chat <<- c(vars$chat, paste0(linePrefix(), "<span class=\"user-change\">\"", 
                                           sanitize(sessionVars$username), 
                                           "\" -> \"", sanitize(input$user), "\"</span>"))
@@ -60,45 +71,55 @@ shinyServer(function(input, output, session) {
         sessionVars$username <- sanitize(input$user)
       })
     }
+    # Add this user to the global list of users
     isolate(vars$users <- c(vars$users, sessionVars$username))
   })
   
-  # Keep the username updated
+  # Keep the username updated with whatever sanitized/assigned username we have
   observe({
     updateTextInput(session, "user", 
                     value=sessionVars$username)    
   })
   
+  # Keep the list of connected users updated
   output$userList <- renderUI({
     tagList(tags$ul( lapply(vars$users, function(user){
       return(tags$li(user))
     })))
   })
   
+  # Listen for input$send changes (i.e. when the button is clicked)
   observe({
     if(input$send < 1){
+      # The code must be initializing, b/c the button hasn't been clicked yet.
       return()
     }
     isolate({
+      # Add the current entry to the chat log.
       vars$chat <<- c(vars$chat, 
                       paste0(linePrefix(), "<span class=\"username\">",
-                             "<abbr title=\"", Sys.time(), "\">", sanitize(input$user),
+                             "<abbr title=\"", Sys.time(), "\">", sessionVars$username,
                              "</abbr></span>: ", sanitize(input$entry)))
     })
+    # Clear out the text entry field.
     updateTextInput(session, "entry", value="")
   })
   
+  # Dynamically create the UI for the chat window.
   output$chat <- renderUI({
     if (length(vars$chat) > 500){
       # Too long, use only the most recent 500 lines
       vars$chat <- vars$chat[(length(vars$chat)-500):(length(vars$chat))]
     }
+    # Save the chat object so we can restore it later if needed.
     saveRDS(vars$chat, "chat.Rds")
+    
+    # Pass the chat log through as HTML
     HTML(vars$chat)
   })
 })
 
-# Replace any HTML tags
+# Replace any HTML tags in user-provided strings to prevent malicious entries.
 sanitize <- function(string){
   str_replace_all(string, "[<>]", "")
 }
