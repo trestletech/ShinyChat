c89ced365475df5a327340b2c449fee70a5ec993
trestletech <jeff.allen@trestletechnology.net>
2014-01-10 13:01:55 -0600

    Define a fixed height for chat and auto-scroll.

--- sendOnEnter.js
+++ sendOnEnter.js
@@ -8,3 +8,21 @@ jQuery(document).ready(function(){
     }
   });
 })
+
+// We don't yet have an API to know when an element is updated, so we'll poll
+// and if we find the content has changed, we'll scroll down to show the new
+// comments.
+var oldContent = null;
+window.setInterval(function() {
+  var elem = document.getElementById('chat');
+  if (oldContent != elem.innerHTML){
+    scrollToBottom();
+  }
+  oldContent = elem.innerHTML;  
+}, 300);
+
+// Scroll to the bottom of the chat window.
+function scrollToBottom(){
+  var elem = document.getElementById('chat');
+  elem.scrollTop = elem.scrollHeight;
+}
